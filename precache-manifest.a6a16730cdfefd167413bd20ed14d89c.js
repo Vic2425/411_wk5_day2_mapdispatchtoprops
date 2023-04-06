4973f3a581f0332be0f477fc05e224a83e19a739
Vic <vicjs612@gmail.com>
2023-04-06 15:29:49 -0500

    Updates

---
+++ precache-manifest.a6a16730cdfefd167413bd20ed14d89c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32e81984f4a200242842eed284b01b5",
+    "url": "/411_wk5_day2_mapdispatchtoprops/index.html"
+  },
+  {
+    "revision": "613c449c1205961d7747",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "e77aad5bb344236fc354",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/2.83831900.chunk.js"
+  },
+  {
+    "revision": "613c449c1205961d7747",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/main.79e41f34.chunk.js"
+  },
+  {
+    "revision": "e47b0be5e9874947a62d",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/runtime~main.df0822c9.js"
+  }
+]);
