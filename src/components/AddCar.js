bafbd92c1aaf5b3bc508f323c906e35d834d419e
Vic <vicjs612@gmail.com>
2023-04-06 15:23:32 -0500

    first commit

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -32,6 +32,7 @@ class AddCar extends Component {
         console.log("THE CAR", payload)
         // add this.props.addCar function here
         // also add this.setState to close the dialog
+        // 'payload' same as 'value'
     }
 
     componentDidUpdate = (prevProps, prevState) => {
